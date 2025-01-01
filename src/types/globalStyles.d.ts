0165abbc1b1664bb830cb87a4168d3cad21cf820
ChriisLM <christianlezcano878@gmail.com>
2024-12-31 23:44:33 -0500

    feat: :sparkles: Add global types

---
+++ src/types/globalStyles.d.ts
@@ -0,0 +1,13 @@
+export type Fonts = "openSans" | "roboto" | "poppins";
+export type FontSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+export type FontWeights = 'light' | 'regular' | 'medium' | 'bold' | 'black';
+export type Spacing = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export type Colors =
+  | "primary"
+  | "secondary"
+  | "error"
+  | "warning"
+  | "info"
+  | "success"
+  | "default";
