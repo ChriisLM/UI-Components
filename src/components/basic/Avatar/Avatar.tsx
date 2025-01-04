ba66b77779686fbf9d83a765d4e7a28e455d6c97
ChriisLM <christianlezcano878@gmail.com>
2025-01-04 17:38:36 -0500

    refactor: :recycle: Refactor avatar component
    -Fix types, fixed styles, and change structure of component

--- src/components/basic/Avatar/Avatar.tsx
+++ src/components/basic/Avatar/Avatar.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import { AvatarProps } from "../../../types/Avatar";
-import "./Avatar.css"
+import "./Avatar.css";
 
 export function Avatar({
   src,
   alt = "Avatar",
   size = "medium",
+  fallback,
+  border,
+  shape = "circle",
   className = "",
+  onClick,
 }: AvatarProps) {
-  const avatarClassName = `avatar avatar-${size} ${className}`.trim();
-  return <img src={src} alt={alt} className={avatarClassName} />;
+  const [error, setError] = useState(false);
+
+  return (
+    <div className={`avatar-container avatar-${size} avatar-${shape} avatar-${border} ${className}`} onClick={onClick}>
+      {!error && src ? (
+        <img
+          src={src}
+          alt={alt}
+          className="avatar"
+          onError={() => setError(true)}
+        />
+      ) : (
+        <span className="avatar-spanError">
+          {fallback || alt.charAt(0).toUpperCase()}
+        </span>
+      )}
+    </div>
+  );
 }
--- src/types/Avatar.d.ts
+++ src/types/Avatar.d.ts
@@ -1,8 +1,15 @@
-export type AvatarSize = 'small' | 'medium' | 'large';
+import { LineStyle } from "./globalStyles";
+
+export type AvatarSize = "small" | "medium" | "large";
+export type AvatarShape = "circle" | "square";
 
 export interface AvatarProps {
   src: string;
   alt?: string;
   size?: AvatarSize;
+  fallback?: string;
+  border?: LineStyle;
+  shape?: AvatarShape;
   className?: string;
+  onClick?: () => void;
 }
