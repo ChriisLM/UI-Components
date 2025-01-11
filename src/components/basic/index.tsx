c1030d676fe7dacefe28f7cd945d1cd96e62103c
ChriisLM <christianlezcano878@gmail.com>
2025-01-11 12:05:49 -0500

    chore: Reorganize project structure for npm package readiness
    - Removed unnecessary files and default templates from Vite setup - Moved and structured global styles for better modularity

---
+++ src/components/basic/index.tsx
@@ -0,0 +1,14 @@
+export { Avatar } from "./Avatar/Avatar";
+export { Badge } from "./Badge/Badge";
+export { Button } from "./Button/Button";
+export { Checkbox } from "./Checkbox/Checkbox";
+export { Divider } from "./Divider/Divider";
+export { Input } from "./Input/Input";
+export { Label } from "./Label/Label";
+export { Radio } from "./Radio/Radio";
+export { Select } from "./Select/Select";
+export { Slider } from "./Slider/Slider";
+export { Switch } from "./Switch/Switch";
+export { Tag } from "./Tag/Tag";
+export { Tooltip } from "./Tooltip/Tooltip";
+export { Typography } from "./Typography/Typography";
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './styles/global.css'
+import './styles/variables.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div>My UI Library</div>
   </StrictMode>,
 )
